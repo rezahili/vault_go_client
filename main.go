2b1c18ceae339d12651abca24fba7b9e7877c902
rezahili <reza1997ghasemi@gmail.com>
2023-03-08 11:28:27 -0500

    write function added

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
-package vault_go_client
+package vault
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+func Write(token string, url string, data map[string]interface{}) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create request with JSON data and header
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	req.Header.Set("X-Vault-Token", token)
+	if err != nil {
+		panic(err)
+	}
+
+	// Send request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+}
